a42b0e31764feb8505cb03ad7c90848bcb2b6797
dg1223 <alavi1223@hotmail.com>
2023-05-01 21:27:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Firebase authentication with context
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmP-0UckEXc1gw8QtKtpyqrjk6jRHyVaw",
+  authDomain: "fir-auth-context-tailwin-5dbdd.firebaseapp.com",
+  projectId: "fir-auth-context-tailwin-5dbdd",
+  storageBucket: "fir-auth-context-tailwin-5dbdd.appspot.com",
+  messagingSenderId: "1013887227438",
+  appId: "1:1013887227438:web:b54f802c1d5bd109dd71bf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
